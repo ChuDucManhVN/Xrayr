69437110fea7f93dcf9306818123ac0975cb8c80
ChuDucManh <89825108+ChuDucManhVN@users.noreply.github.com>
2022-09-27 17:02:38 +0700

    Update raw.sh

--- raw.sh
+++ raw.sh
@@ -2,6 +2,5 @@ sudo ufw allow 80/tcp
 sudo ufw allow 443/tcp
 sudo ufw allow 80
 sudo ufw allow 443
-sudo bash <(curl -Ls https://xrayvn.xyz/install.sh)
 git clone https://github.com/ChuDucManhVN/Xrayr
 sh Xrayr/run.sh
