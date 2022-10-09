bb3870db324e584f94a28afcbb190641443bb517
ChuDucManh <89825108+ChuDucManhVN@users.noreply.github.com>
2022-10-09 20:09:53 +0700

    Add files via upload

---
+++ config/HinhTron.py
@@ -0,0 +1,45 @@
+from re import A
+from time import sleep
+
+re=1
+
+print("███╗░░░███╗░█████╗░███╗░░██╗██╗░░██╗")
+print("████╗░████║██╔══██╗████╗░██║██║░░██║")
+print("██╔████╔██║███████║██╔██╗██║███████║")
+print("██║╚██╔╝██║██╔══██║██║╚████║██╔══██║")
+print("██║░╚═╝░██║██║░░██║██║░╚███║██║░░██║")
+print("╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝░░╚═╝")
+print(" ")
+
+while re==1:
+
+    x=str(input("Sử dụng đường kính(bk) hay bán kính(dk): "))
+    if x=="dk":
+        dk=float(input("Đường kính: "))
+        dt=3.14*dk/2*dk/2
+        cv=3.14*dk
+        print("Chu vi: " , (round(cv)))
+        print("Diện tích: " , (round(dt)))
+        print("Xong")
+        re=re+1
+        rere=str(input("Có muốn tính lại không(y/n): "))
+        if rere=="y":
+            re=re-1
+        if rere=="n":
+            print("BYE")
+            sleep(0.5)
+
+    if x=="bk":
+        bk=float(input("Bán kính: "))
+        cv1=3.14*bk*2
+        dt1=3.14*bk**2
+        print("Chu vi: " , (round(cv1,4)))
+        print("Diện tích: " , (round(dt1)))
+        print("Xong")
+        re=re+1
+        rerere=str(input("Có muốn tính lại không(y/n): "))
+        if rerere=="y":
+            re=re-1
+        if rerere=="n":
+            print("BYE")
+            sleep(0.5)
---
+++ config/ping.py
@@ -0,0 +1,41 @@
+from time import sleep
+import subprocess
+import platform
+
+print("███╗░░░███╗░█████╗░███╗░░██╗██╗░░██╗")
+print("████╗░████║██╔══██╗████╗░██║██║░░██║")
+print("██╔████╔██║███████║██╔██╗██║███████║")
+print("██║╚██╔╝██║██╔══██║██║╚████║██╔══██║")
+print("██║░╚═╝░██║██║░░██║██║░╚███║██║░░██║")
+print("╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝░░╚═╝")
+print(" ")
+
+y=3
+
+while y==3:
+
+    operating_sys = platform.system()
+
+    def ping(ip):
+        ping_command = ['ping', ip, '-n', '1'] if operating_sys == 'Windows' else ['ping', ip, '-c 1']
+        shell_needed = True if operating_sys == 'Windows' else False
+    
+        ping_output = subprocess.run(ping_command,shell=shell_needed,stdout=subprocess.PIPE)
+        success = ping_output.returncode
+        tr= ip + " : " + "Online"
+        fl= ip + " : " + "TimeOut"
+        return tr if success == 0 else fl
+
+    print(ping("1.vpscuamanh.tk"))
+    print(ping("2.vpscuamanh.tk"))
+    print(ping("3.vpscuamanh.tk"))
+    print(ping("4.vpscuamanh.tk"))
+    print(ping("manhchu.cf"))
+    y=y+1
+    print(" ")
+    re=str(input("Có muốn ping lại không(y/n): "))
+    if re=="y":
+        y=y-1
+    if re=="n":
+        print("BYE")
+        sleep(0.5)
